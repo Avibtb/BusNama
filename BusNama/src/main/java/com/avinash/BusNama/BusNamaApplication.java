d5e765f50e032ac4e304a23d8a7e8317442dcc6e
Avibtb <avi.singh.iit01@gmail.com>
2023-07-30 11:18:53 +0530

    Project Boilerplate

---
+++ BusNama/src/main/java/com/avinash/BusNama/BusNamaApplication.java
@@ -0,0 +1,13 @@
+package com.avinash.BusNama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusNamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusNamaApplication.class, args);
+	}
+
+}
---
+++ BusNama/src/test/java/com/avinash/BusNama/BusNamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.BusNama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusNamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
